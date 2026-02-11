f0078091d6bccfc85d0be4aeaad95708b9975360
Patrick <patrickcastn@gmail.com>
2026-02-11 10:49:55 -0300

    fix: Configura o build de lançamento para não ser depurável e remove a assinatura de depuração.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -32,10 +32,8 @@ android {
 
 
     buildTypes {
-        release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+        getByName("release") {
+            isDebuggable = false
         }
     }
     flavorDimensions("default")
